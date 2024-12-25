46d3014848c1340001a8915e6e39f88eeff57f77
faracutrisfa <faracutrisfa@gmail.com>
2024-12-25 19:31:16 +0700

    feat: add some component and library

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from "../components/HomePage/Hero"
 import Category from "../components/HomePage/Category"
+import MostProduct from '../components/HomePage/MostProduct'
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
       <Category />
+      <MostProduct />
     </div>
   )
 }
