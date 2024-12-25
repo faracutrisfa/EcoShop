0ed66576889be10f7381192feaf635930fae4e7a
faracutrisfa <faracutrisfa@gmail.com>
2024-12-25 18:55:46 +0700

    style: add styling hero component

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import PrimaryButton from '../PrimaryButton';
+import heroImage from "../../assets/HomePage/hero_image.webp"
 
 const Hero = () => {
+    const handleClick = () => {
+        alert("Button clicked!");
+    };
+
     return (
-        <div>
-            INI HERO
-        </div>
+        <section className='px-4 lg:px-16 flex justify-between'>
+            <div className='space-y-6 text-center lg:text-start pl-16 py-20'>
+                <h1 className='text-grey-600 font-bold text-4xl lg:text-5xl font-libre'>Healthy cutlery,  healthy body</h1>
+                <p className='text-grey-500 text-sm lg:text-xl font-inter leading-7'>
+                    Upgrade your dining experience and <br/>
+                    begin your path to a healthy you right now.
+                </p>
+                <PrimaryButton onClick={handleClick}>Learn More</PrimaryButton>
+            </div>
+            <div>
+                <img src={heroImage} alt="image" className='w-[720px]' />
+            </div>
+        </section>
     )
 }
 
