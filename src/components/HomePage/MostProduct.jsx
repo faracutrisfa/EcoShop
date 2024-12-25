bbe99b5676fd094fe4e65a49810169c22a8170c0
faracutrisfa <faracutrisfa@gmail.com>
2024-12-25 19:29:55 +0700

    feat: add title and subtitle for this component

--- src/components/HomePage/MostProduct.jsx
+++ src/components/HomePage/MostProduct.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const MostProduct = () => {
     return (
-        <div>
-            INI MOST PRODUCT
-        </div>
+        <section className='bg-secondary'>
+            <div className='px-4 lg:px-16 py-20'>
+                <div className='flex flex-col justify-center text-center gap-2'>
+                    <h1 className='text-grey-600 text-3xl lg:text-5xl font-bold font-libre'>Our Most Wanted Products</h1>
+                    <h2 className='text-sm lg:text-xl text-grey-500 leading-tight lg:leading-7 font-inter'>Good for environment, good for you</h2>
+                </div> 
+            </div>
+        </section>
     )
 }
 
