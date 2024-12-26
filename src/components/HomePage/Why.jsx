20f9555fb57dd17323afc2efb43bbf6f73a8e809
faracutrisfa <faracutrisfa@gmail.com>
2024-12-26 16:00:21 +0700

    feat: add why component

---
+++ src/components/HomePage/Why.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import WhyCard from '../WhyCard'
+
+const Why = () => {
+    return (
+        <section>
+            <div className='px-4 lg:px-16 py-20 lg:py-40 space-y-20'>
+                <h1 className='text-primary text-3xl lg:text-5xl font-bold font-libre text-center'>Why Bought From Us?</h1>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Why
