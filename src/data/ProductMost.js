3e75befce349f4d36c9b7935eecf73d04662f4d7
faracutrisfa <faracutrisfa@gmail.com>
2024-12-26 16:01:26 +0700

    feat: add data for sample data

---
+++ src/data/ProductMost.js
@@ -0,0 +1,26 @@
+export const ProductMost = [
+    {
+      image: "src/assets/product/hand_soap.png",
+      title: "Alpine Provisions | Cedar + Sandalwood Hand Soap",
+      price: "Rp99.900",
+      discountPrice: "Rp259.000",
+    },
+    {
+      image: "src/assets/product/slippers.png",
+      title: "Women's Classic Sole Low Back Wool Slippers",
+      price: "Rp79.900",
+      discountPrice: "Rp159.900",
+    },
+    {
+      image: "src/assets/product/basket.png",
+      title: "Large Patterned Hamper Basket",
+      price: "Rp199.900",
+      discountPrice: "Rp329.900",
+    },
+    {
+      image: "src/assets/product/powder.png",
+      title: "Meliora | Eco Laundry Powder",
+      price: "Rp159.000",
+      discountPrice: "Rp259.000",
+    },
+  ];  
