d4abcf6ccf98d3b5d85b0f43ab86ee702d6101db
faracutrisfa <faracutrisfa@gmail.com>
2024-12-26 15:58:56 +0700

    style: add style most product component

--- src/components/HomePage/MostProduct.jsx
+++ src/components/HomePage/MostProduct.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from 'react';
+import ProductCard from '../ProductCard';
+import { ProductMost } from '../../data/ProductMost';
 
 const MostProduct = () => {
     return (
-        <section className='bg-secondary'>
-            <div className='px-4 lg:px-16 py-20'>
+        <section className='bg-secondaryLight'>
+            <div className='px-4 lg:px-16 py-20 space-y-10'>
                 <div className='flex flex-col justify-center text-center gap-2'>
                     <h1 className='text-grey-600 text-3xl lg:text-5xl font-bold font-libre'>Our Most Wanted Products</h1>
                     <h2 className='text-sm lg:text-xl text-grey-500 leading-tight lg:leading-7 font-inter'>Good for environment, good for you</h2>
-                </div> 
+                </div>
+                <div className='flex justify-center'>
+                    <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+                        {ProductMost.map((product, index) => (
+                            <ProductCard
+                                key={index}
+                                image={product.image}
+                                title={product.title}
+                                price={product.price}
+                                discountPrice={product.discountPrice}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default MostProduct
+export default MostProduct;
