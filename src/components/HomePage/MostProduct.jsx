142d0296c20f2000e602851094744e43506c639f
faracutrisfa <faracutrisfa@gmail.com>
2024-12-27 20:21:29 +0700

    style: add responsive most product style

--- src/components/HomePage/MostProduct.jsx
+++ src/components/HomePage/MostProduct.jsx
@@ -5,7 +5,7 @@ import { ProductMost } from '../../data/ProductMost';
 const MostProduct = () => {
     return (
         <section className='bg-secondaryLight'>
-            <div className='px-4 lg:px-16 py-20 space-y-10'>
+            <div className='px-4 lg:px-16 py-10 lg:py-20 space-y-10'>
                 <div className='flex flex-col justify-center text-center gap-2'>
                     <h1 className='text-grey-600 text-3xl lg:text-5xl font-bold font-libre'>Our Most Wanted Products</h1>
                     <h2 className='text-sm lg:text-xl text-grey-500 leading-tight lg:leading-7 font-inter'>Good for environment, good for you</h2>
