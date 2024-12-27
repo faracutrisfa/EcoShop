0ac0fa4ee8dbdd11d53561efb24e6df1542ea60d
faracutrisfa <faracutrisfa@gmail.com>
2024-12-27 20:21:49 +0700

    feat: add new arrival component

--- src/components/HomePage/NewArrival.jsx
+++ src/components/HomePage/NewArrival.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const NewArrival = () => {
     return (
-        <div>
-            new
-        </div>
+        <section>
+            <div className='px-4 md:px-10 lg:px-16 py-10 lg:py-20'>
+                <div className='flex flex-col gap-2'>
+                    <h1 className='text-grey-600 text-2xl lg:text-4xl font-bold font-libre'>New Arrival</h1>
+                    <h2 className='text-sxslg:text-base text-grey-500 leading-tight lg:leading-7 font-inter'>Be the first to have the first-class product</h2>
+                </div>
+            </div>
+        </section>
     )
 }
 
