714ef6d9f1b4922b82f7d7fa8695cfd82f5ef001
faracutrisfa <faracutrisfa@gmail.com>
2025-01-01 22:30:52 +0700

    style: add responsive text for quotes component

--- src/components/HomePage/Quotes.jsx
+++ src/components/HomePage/Quotes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Quotes = () => {
     return (
         <div className='px-4 md:px-10 lg:px-16 bg-primary py-[120px]'>
-            <h1 className='text-center text-3xl font-bold font-libre text-white '>Shop guilt-free knowing that our eco-friendly e-commerce platform is committed to reducing environmental impact and promoting sustainable practices in every step of the shopping process</h1>
+            <h1 className='text-center text-xl lg:text-3xl font-bold font-libre text-white '>Shop guilt-free knowing that our eco-friendly e-commerce platform is committed to reducing environmental impact and promoting sustainable practices in every step of the shopping process</h1>
         </div>
     )
 }
